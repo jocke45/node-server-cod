0345431c1d64d0a71632af5301d408be5206de13
Jocke45 <jocke45@gmail.com>
2020-10-21 17:08:47 +0200

    Fixed so help command is dynamic

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,24 @@
+//const fs = require('fs');
 const Discord = require('discord.js');
 
 module.exports = {
     name: 'help',
-    description: 'List all available commands',
+    description: 'List all available commands and how to use',
     args: false,
     execute(message, args) {
+        const { commands } = message.client;
         const embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Available commands')
             .setThumbnail('https://img.icons8.com/plasticine/100/000000/question-mark.png')
-            .addFields(
-                { name: '!help', value: 'Show this help message' },
-                { name: '!kills *player ID*', value: 'Display the number of kills for the player' },
-                { name: '!wins *player ID*', value: 'Display how many times the player has won' },
-                { name: '!ping', value: 'Pong' },
-            )
             .setTimestamp()
             .setFooter('Icon by Icons8', 'https://icons8.com');
+        // Loop over all commands and print their description
+        commands.forEach(cmd => {
+            let tempName = cmd.name;
+            if (cmd.args) tempName += ' *player_id*';
+            embed.addField(`!${tempName}`, `${cmd.description}`);
+        });
         return message.channel.send(embed)
     },
 };
